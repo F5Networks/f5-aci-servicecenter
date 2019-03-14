18ef92b935dad741c805778517d2c143da19fc4b
Andrey KASHCHEEV <kashcheev@sea-ml-00027379.olympus.f5net.com>
2019-03-14 15:21:17 -0700

    update in CI workflow

--- script/test-docs.sh
+++ script/test-docs.sh
@@ -5,11 +5,11 @@ set -x
 : ${DOC_IMG:=f5devcentral/containthedocs:latest}
 
 exec docker run -i \
-  -v $PWD:/here --workdir /here \
+  -v $PWD:$PWD --workdir $PWD \
   ${DOC_IMG} /bin/bash -s <<EOF
 set -e
 #echo "Installing project dependencies"
-pip install --user -r requirements.readthedocs.txt
+pip install --user -r requirements.txt
 # - needed to put the inv command in path
 export PATH=$PATH:/root/.local/bin
 echo "Building docs with Sphinx"
